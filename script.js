5c2d5e6517b53aa631a33f3098d1020ec0a4b322
salmasweid <swedsalma02@gmail.com>
2024-05-17 12:33:33 +0300

    completed

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("[data-js-form]");
+const template = document.querySelector("[data-js-template]");
+const wrapper = document.querySelector("[data-js-wrapper");
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const rating = new FormData(event.target).get("rating");
+
+    if(rating) {
+        wrapper.innerHTML = template.innerHTML.replace(/{{ rating }}/, rating);
+    }
+    else {
+        alert("Please select a rating");
+    }
+}
+
+form.addEventListener("submit", handleSubmit);
